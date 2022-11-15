c7c09b7f7af70335afe7770ba9bd1c68076d573a
Jiri Zak <jiri.zak@bringauto.com>
2022-11-15 15:00:31 +0100

    get_command update in command manager

--- lib/module_api/include/_internal_server/command_manager.h
+++ lib/module_api/include/_internal_server/command_manager.h
@@ -16,9 +16,11 @@ void update_command(void *command_message);
  * After providing a status message, the actual command message is updated and returned.
  *
  * @param status_message new status message
+ * @param buffer pointer to an user allocated buffer, command message structure will be copied to it, structures are defined in device specific header file
+ * @param buffer_size size of user allocated buffer
  *
- * @return command message if successes, null pointer if an error occurred
+ * @return size of command, -1 if status_message is incorrect, -2 if buffer is too small, -3 for other error
  */
-void *get_command(void *status_message);
+int get_command(void *status_message, void *buffer, unsigned buffer_size);
 
 #endif //MODULE_API_COMMAND_MANAGER_H
