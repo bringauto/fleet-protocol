59c3cb0eecbead7f1869ad054e3bf8e5566e934b
melkus <josefmelkus@seznam.cz>
2022-12-22 12:05:48 +0100

    refactor

--- examples/protobuf_parsing_example/main.cpp
+++ examples/protobuf_parsing_example/main.cpp
@@ -4,14 +4,7 @@
 #include <ProtocolMaintainer.hpp>
 #include <ModuleMaintainer.hpp>
 
-
-
-int main(int argc, char **argv) {
-	// creating device
-	auto device = ProtocolMaintainer::createDevice("leftBlinker", ModuleMaintainer::BLINKER, "blinkerA1"); // Internal Client
-
-	// Connection //
-
+void serializeConnectMessages(const InternalProtocol::Device& device) {
 	auto connectMessage = ProtocolMaintainer::createDeviceConnectMessage(device, 1); // Internal Client
 	ProtocolMaintainer::parseDeviceConnectMessage(connectMessage); // Internal Server
 
@@ -24,9 +17,9 @@ int main(int argc, char **argv) {
 	auto externalConnect = ProtocolMaintainer::createConnectMessage("BringAuto", "Vehicle1", devices);
 
 	ProtocolMaintainer::parseExternalClientMessage(externalConnect);
+}
 
-	// Statuses //
-
+void serializeStatusMessages(const InternalProtocol::Device& device) {
 	auto blinkerStatus = ModuleMaintainer::createBlinkerStatus(true); // Device
 
 	std::string bytes = blinkerStatus.SerializeAsString();	// Message must be serialized as string, so it can be put into bytes field
@@ -36,10 +29,9 @@ int main(int argc, char **argv) {
 
 	auto deviceStatusParsed = ProtocolMaintainer::parseExternalClientMessage(status); // internal server / external server
 	ModuleMaintainer::parseDeviceStatus(deviceStatusParsed); // device / cloud application
+}
 
-
-	// Commands //
-
+void serializeCommandMessages(const InternalProtocol::Device& device) {
 	auto blinkerCommand = ModuleMaintainer::createBlinkerCommand(ExampleModule::BlinkerCommand_Command_TURN_OFF); // Cloud application for controlling device
 	auto deviceCommand = ProtocolMaintainer::createDeviceCommand(blinkerCommand.SerializeAsString()); // External server / Internal Client
 
@@ -48,5 +40,13 @@ int main(int argc, char **argv) {
 	auto commandData = ProtocolMaintainer::parseExternalServerMessage(command); // parse all parts of the command, External Client
 	ModuleMaintainer::parseBlinkerCommand(commandData); // called based on deviceType in the Command, the Command sent to Device doesn't have device identification
 
+}
 
+int main(int argc, char **argv) {
+	// creating device
+	auto device = ProtocolMaintainer::createDevice("leftBlinker", ModuleMaintainer::BLINKER, "blinkerA1"); // Internal Client
+
+	serializeConnectMessages(device);
+	serializeStatusMessages(device);
+	serializeCommandMessages(device);
 }
